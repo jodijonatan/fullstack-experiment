38ff5d6905aafa595cdf750cf5cd6f8061cdfab8
Jodi Jonatan <jdi102350@gmail.com>
2025-09-02 10:42:07 +0700

    fix header logic

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+
+session_start();
+
+if(isset($_SESSION['is_login'])) {
+  header('location: dashboard.php');
+  exit;
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
--- layouts/header.php
+++ layouts/header.php
@@ -1,8 +1,8 @@
 <header class="bg-white flex justify-between px-8 h-16 items-center">
   <h2 class="font-bold text-2xl text-sky-800">Fullstack Experiment</h2>
   <ul class="flex text-lg font-semibold">
-    <li><a class="ml-8 hover:underline hover:text-sky-800" href="index.php">Home</a></li>
     <?php if(!isset($_SESSION['is_login']) || $_SESSION['is_login'] == false) { ?>
+      <li><a class="ml-8 hover:underline hover:text-sky-800" href="index.php">Home</a></li>
       <li><a class="ml-8 hover:underline hover:text-sky-800" href="login.php">Login</a></li>
       <li><a class="ml-8 hover:underline hover:text-sky-800" href="register.php">Register</a></li>
     <?php } ?>
